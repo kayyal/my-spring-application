b591f7919de38b41d74a1cbad0d72f7ab491a1bc
amir kayyal <amir.kayyal@gmail.com>
2023-09-10 13:09:19 +0330

    Refactored RedisConfig to read redis address from application.yaml

--- src/main/java/ir/neshan/myspringapplication/config/RedisConfig.java
+++ src/main/java/ir/neshan/myspringapplication/config/RedisConfig.java
@@ -3,17 +3,21 @@ package ir.neshan.myspringapplication.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${redis.address}")
+    private String redisAddress;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress(redisAddress);
 
         return Redisson.create(config);
     }
